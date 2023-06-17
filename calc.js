24ef9ccccff7621f68f388da5ee20d0780182b04
major6977 <majordav@msn.com>
2023-06-17 13:25:38 -0700

    first commit

---
+++ calc.js
@@ -0,0 +1,7 @@
+const clearBtn = document.querySelector("#clearBtn");
+
+clearBtn.addEventListener("click", clearDisplay);
+function clearDisplay() {
+  console.log("Clearing display");
+}
+// clearDisplay();
