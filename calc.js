2b0a80dd5df4b33d7b619ed5381776428ed026e3
major6977 <majordav@msn.com>
2023-06-18 10:16:58 -0700

    added javascript

--- calc.js
+++ calc.js
@@ -2,6 +2,22 @@ const clearBtn = document.querySelector("#clearBtn");
 
 clearBtn.addEventListener("click", clearDisplay);
 function clearDisplay() {
-  console.log("Clearing display");
+  thePoint.textContent = "";
+  // console.log("Clearing display");
 }
+
+function topDisplay() {
+  const displayContainer = document.querySelector("#displayContainer");
+  const theDisplay = document.createElement("div");
+  theDisplay.className = "theDisplay";
+  thePoint = document.createElement("p");
+  thePoint.className = "thePoint";
+  thePoint.style.color = "red";
+  thePoint.textContent = "HELLO WORLD";
+  theDisplay.appendChild(thePoint);
+  displayContainer.appendChild(theDisplay);
+  console.log(theDisplay);
+}
+topDisplay();
+
 // clearDisplay();
